0124fc1ce4c92af420d2ea931514e76bc7bbdcba
Adnan Muratovic <adnannmuratovic@icloud.com>
2025-09-01 19:29:40 +0200

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Adnann Muratovic on 01.09.25.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsTests/NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTests.swift
+//  NewsTests
+//
+//  Created by Adnann Muratovic on 01.09.25.
+//
+
+import Testing
+@testable import News
+
+struct NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
